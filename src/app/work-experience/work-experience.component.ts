50624b284fa578ef050f6719baf8ed5ba104b0d2
Jose Cinco <a00227714@tec.mx>
2023-02-15 17:04:15 -0600

    init cv v 1.1

--- src/app/work-experience/work-experience.component.ts
+++ src/app/work-experience/work-experience.component.ts
@@ -7,4 +7,39 @@ import { Component } from '@angular/core';
 })
 export class WorkExperienceComponent {
 
+  workExperience : Array<any> = [];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    let work1 = {
+      fecha : "2021-2022",
+      ubicacion : "Monterrey, Nuevo Leon",
+      puesto : "System Administrator",
+      empresa: "Tec de Monterrey",
+      logros : [
+        {descripcion : "Administrar un sistema de mas de cinco mil estudiantes"},
+        {descripcion: "Creacion de paneles de Salud del Sistema"}
+      ]
+    };
+
+    let work2 = {
+      fecha : "Abr-Jun 2022",
+      ubicacion : "Monterrey, Nuevo Leon",
+      puesto : "Full Stack Developer",
+      empresa: "ATOS",
+      logros : [
+        {descripcion : "Realizacion de un Sitio Web completo para registrar interesados en la empresa (jobs)."},
+        {descripcion: "Realizado en .NET"}
+      ]
+    };
+
+    this.workExperience.push(work1);
+    this.workExperience.push(work2);
+
+
+
+  }
+
+
 }
