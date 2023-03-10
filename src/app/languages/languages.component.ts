8a0ce3802142b5ce3ce4ee561743134809aa10ff
JoseMiguel <a00227714@tec.mx>
2023-03-10 17:34:08 -0600

    Added Languages Section to CV

--- src/app/languages/languages.component.ts
+++ src/app/languages/languages.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class LanguagesComponent {
 
+  languages : Array<any> = [];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    let l1 = {
+      name : "Spanish",
+      skill : "Native"
+    };
+
+    let l2 = {
+      name : "English",
+      skill : "Advanced"
+    };
+
+
+    this.languages.push(l1);
+    this.languages.push(l2);
+
+  }
 }
